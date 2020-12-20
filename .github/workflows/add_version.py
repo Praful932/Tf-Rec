f28396fd31f71b07a6792cc40ceaf79ad24c00c4
Praful932 <praful.mohanan@gmail.com>
2020-12-20 13:12:23 +0530

    Update testpypi to pypi for first release

--- .github/workflows/add_version.py
+++ .github/workflows/add_version.py
@@ -5,7 +5,7 @@ new_version = str(sys.argv[1])
 
 with open("setup.py") as f:
     lines = f.readlines()
-lines[4] = "version = '" + new_version + "'\n"
+lines[4] = "version = '" + new_version[1:] + "'\n"
 
 with open("setup.py", "w") as f:
     f.writelines(lines)
